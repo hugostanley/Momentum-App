88a25715f6c178c99f89c458f455bd8df759ac47
hugostanley <stanleyhugo06@gmail.com>
2022-05-30 22:29:00 +0800

    Add JavaScript to the background image

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,26 @@
 let currentTime = new Date()
 let hours = currentTime.getHours()
-let minutes = currentTime.getMinutes()
+let minutes = currentTime.toLocaleString('en-US', {
+   minute: 'numeric',
+})
+
 let time = document.querySelector('.center-region_time')
 
+let greetText = document.querySelector('.greet')
+
 function timeNow() {
-   if (hours > 12) {
+   if (hours >= 12 && hours <= 6) {
       hours = hours % 12
       time.innerText = `${hours}:${minutes}`
-   } else {
+      greetText.textContent += ' Afternoon'
+   } else if (hours >= 19 && hours < 24) {
+      hours = hours % 12
       time.innerText = `${hours}:${minutes}`
+      greetText.textContent += ' Evening'
+   } else if (hours >= 1 && hours <= 11) {
+      time.innerText = `${hours}:${minutes}`
+      greetText.textContent = +' Morning'
    }
 }
 
 timeNow()
-
