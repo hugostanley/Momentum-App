b70a911a039a669e2dbe283e6687c67245da3a26
hugostanley <stanleyhugo06@gmail.com>
2022-05-31 20:29:55 +0800

    Create a refresh buton that randomizes the quotes and store the input in the local storage

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 let currentTime = new Date()
 let hours = currentTime.getHours()
 let minutes = (currentTime.getMinutes() < 10 ? '0' : '') + currentTime.getMinutes()
-
+let main = document.querySelector('main')
 let time = document.querySelector('.center-region_clock')
 
 let greetText = document.querySelector('.greet')
@@ -10,46 +10,21 @@ function timeNow() {
    if (hours >= 12 && hours <= 18) {
       hours = hours % 12
       time.innerText = `${hours}:${minutes}`
-      greetText.textContent += ' Afternoon'
+      greetText.textContent += 'Good Afternoon'
+      main.style.backgroundImage = 'url(../images/forest.jfif)'
    } else if (hours >= 19 && hours < 24) {
       hours = hours % 12
       time.innerText = `${hours}:${minutes}`
-      greetText.textContent += ' Evening'
+      greetText.textContent += 'Good Evening'
+      main.style.backgroundImage = 'url(../images/darkmountain.jpg)'
    } else if (hours >= 1 && hours <= 11) {
       time.innerText = `${hours}:${minutes}`
-      greetText.textContent += ' Morning'
+      greetText.textContent += 'Good Morning'
+      main.style.backgroundImage = 'url(../images/sunrise.jpg)'
    }
 }
 
 timeNow()
-
-// const userName = document.querySelector('#name')
-// const nameQu = document.querySelector('.label.name')
-
-// userName.addEventListener('keypress', getName)
-
-// function getName(e) {
-//    function storeName() {
-//       if (e.key === 'Enter') {
-//          // code for enter
-//          let userOne = userName.value
-//          console.log(userOne)
-
-//          localStorage.setItem('userName', userOne)
-//          let newUser = localStorage.getItem('userName')
-//          greetText.textContent += `, ${newUser}`
-//       }
-//    }
-
-//    function changeBg() {
-//       document.querySelectorAll('.label.name', '.input.name').style.cssText =
-//          'visibility: hidden; opacity: 0;'
-//       u
-//    }
-
-//    storeName()
-//    changeBg()
-// }
 
 const nameModal = document.querySelector('.modal.name')
 const focusModal = document.querySelector('.modal.focus')
@@ -58,7 +33,12 @@ const inputFocus = document.querySelector('.input.focus')
 const focusTextModal = document.querySelector('.modal.focus-text')
 let focusText = document.querySelector('.focusText')
 inputName.addEventListener('keypress', openFocus)
-console.log(openFocus())
+
+// const storedName = localStorage.getItem('username')
+
+// if (storedName) {
+//    greetText.textContent = storedName
+// }
 
 function openFocus(e) {
    if (e.key === 'Enter') {
@@ -66,14 +46,32 @@ function openFocus(e) {
       let userName = inputName.value
       greetText.textContent += `, ${userName}`
       inputFocus.addEventListener('keypress', displayFocus)
+      localStorage.setItem('username', greetText.textContent)
 
       function displayFocus(e) {
          if (e.key === 'Enter') {
             let focus = inputFocus.value
             focusText.textContent = focus
-
+            localStorage.setItem('mainFocus', focusText.textContent)
             focusTextModal.show()
          }
       }
    }
+}
+
+const refresh = document.querySelector('.refresh-icon')
+const para = document.querySelector('.text-quote')
+
+let quotes = [
+   '"Don\'t let yesterday take too much of today."',
+   '"Happiness is not the absence of problems, it is the ability to deal with them."',
+   '"There is nothing that works out for the worst that won\'t work out for the better."',
+   '"The hardes shot you\'d ever take is the one that you didn\'t take."',
+]
+
+refresh.addEventListener('click', randomize)
+
+function randomize() {
+   let randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+   para.innerHTML = randomQuote
 }
